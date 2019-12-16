1a1ae9c99458934e70cd927886ed80561e52a64a
CheahZhiChing <56948582+CheahZhiChing@users.noreply.github.com>
2019-12-16 12:11:23 +0800

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer = 0
+        Do While x < 20
+            x = x + 1
+            Console.WriteLine(x)
+        Loop
+        Console.ReadLine()
+
+    End Sub
+
+End Module
